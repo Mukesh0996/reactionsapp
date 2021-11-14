8d15002d427b9446eb633a3fbd2593efb35fba88
Mukesh <mukeshkeerthipati96@gmail.com>
2021-11-14 16:41:15 +0530

    Update reaction.js

--- src/components/Reaction/Reaction.js
+++ src/components/Reaction/Reaction.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useContext } from 'react/cjs/react.development';
+import React, { useContext } from 'react';
 import { StoreContext } from '../../store/store';
 import Emoji from '../Emoji/Emoji';
 import styles from './Reaction.module.css';
